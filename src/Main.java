3638ec1a382274ab3fbb2fea0b47c8cc212edc63
deethompson <devon.thompson0@gmail.com>
2019-09-13 11:42:34 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -16,35 +16,50 @@ public class Main {
         Random  c = new Random();
 
 
-        String[] rice = {"white rice, ", "brown rice, ", "no rice, all rice "};
+        String[] rice = {"white rice, ", "brown rice, ", "no rice,","all rice "};
         String[] meat = new String[]{"chicken,", "steak", "carnidas,", "chorizo,", "sofritas,", "veggies,", "none,", "all,"};
         String[] beans = {"pinto, ", "black, ", "none,"};
-        String[] salsa = {"mild, ", "medium, ", "hot,", "none,", "all,"};
-        String[] veggies = {"mild, ", "medium, ", "hot,", "none,", "all,"};
-        String[] cheese = {"yes, ", "no,"};
-        String[] guac = {"yes, ", "no,"};
-        String[] queso = {"yes, ", "no,"};
-        String[] sourcream = {"yes, ", "no,"};
+        String[] salsa = {"mild, ", "medium, ", "hot,", "no salsa,", "all salsa,"};
+        String[] veggies = {"mild, ", "medium, ", "hot,", "no veggies,", "all veggies,"};
+        String[] cheese = {" add cheese, ", "no cheese,"};
+        String[] guac = {"add quac, ", "no guac,"};
+        String[] queso = {"add queso, ", "no queso,"};
+        String[] sourcream = {"add sourcream, ", "no sourcream,"};
 
         for (int i = 0; i < 26; i++) {
             //for (int j = 0; j < 26; j++) {
-                System.out.print("Burrito "+i+": ");
-                System.out.print(rice[r.nextInt(3)]+",");
-                System.out.print(meat[m.nextInt(5)]+", " );
-                System.out.print(beans[b.nextInt(2)]+", ");
-                System.out.print(salsa[s.nextInt(4)]+", ");
-                System.out.print(cheese[c.nextInt(2)]+", ");
-                System.out.print(cheese[q.nextInt(2)]+", ");
-                System.out.print(queso[q.nextInt(2)]+",");
-                System.out.print(sourcream[sc.nextInt(2)]+", ");
+            System.out.print("Burrito "+i+": ");
+            System.out.print(rice[r.nextInt(3)]+",");
+            System.out.print(meat[m.nextInt(5)]+", " );
+            System.out.print(beans[b.nextInt(2)]+", ");
+            System.out.print(salsa[s.nextInt(4)]+", ");
+            System.out.print(cheese[c.nextInt(2)]+", ");
+            System.out.print(cheese[q.nextInt(2)]+", ");
+            System.out.print(queso[q.nextInt(2)]+",");
+            System.out.print(sourcream[sc.nextInt(2)]+", ");
+            int norice = 0;
+            int allrice = 0;
 
-
-
-                System.out.println();
+            if ( norice == allrice )
+            {
+                System.out.println( " " );
+            }
+            if ( norice != allrice )
+            {
+                System.out.println( " " );
             }
         }
-
     }
+
+
+        }
+
+
+
+
+
+
+
 
 
 
